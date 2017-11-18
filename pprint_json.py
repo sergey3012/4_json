2edb13a415cd69ca64f7d7ae6b6128d0bffd136c
sergey3012 <33592950+sergey3012@users.noreply.github.com>
2017-11-18 22:24:48 +0300

    внесены изменения в код

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,22 @@
 import json
+import os
+from pprint import pprint
 
 
-def load_data(filepath):
-    pass
+filename = 'alco_shops.json'
 
 
-def pretty_print_json(data):
-    pass
+def load_data(filename):
+    if not os.path.exists(filename):
+        return None
+    with open (filename, 'r', encoding='utf-8') as f:
+        return json.load(f)
+
+
+def pretty_print_json(filename):
+        return pprint(data)
 
 
 if __name__ == '__main__':
-    pass
+    data = load_data(input('Введите название/адрес файла: '))
+    pretty_print_json(data)

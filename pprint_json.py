f70618fd71bf39ece730c2e83879ccdd9f75ca2f
sergey3012 <33592950+sergey3012@users.noreply.github.com>
2017-11-19 10:54:52 +0300

    внесены исправления
    проверены отступы
    переименована функция load_data на  load_data_json
    переименована переменная f на json_file

--- pprint_json.py
+++ pprint_json.py
@@ -6,17 +6,17 @@ from pprint import pprint
 filename = 'alco_shops.json'
 
 
-def load_data(filename):
+def load_data_json(filename):
     if not os.path.exists(filename):
         return None
-    with open (filename, 'r', encoding='utf-8') as f:
-        return json.load(f)
+    with open (filename, 'r', encoding='utf-8') as json_file:
+        return json.load(json_file)
 
 
 def pretty_print_json(filename):
-        return pprint(data)
+    return pprint(data)
 
 
 if __name__ == '__main__':
-    data = load_data(input('Введите название/адрес файла: '))
+    data = load_data_json(input('Введите название/адрес файл: '))
     pretty_print_json(data)

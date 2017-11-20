dd019ab55d9896a52607ceebe4a79f86551f04ce
sergey3012 <33592950+sergey3012@users.noreply.github.com>
2017-11-20 22:54:26 +0300

    Update pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -1,23 +1,20 @@
 import json
 import os
-from pprint import pprint
 
 
-json_filename = 'alco_shops.json'
-
-
-def load_data_json(json_filename):
-    if not os.path.exists(json_filename):
-        return None
-    with open (json_filename, 'r', encoding='utf-8') as json_file:
+def load_data_json(filepath):
+    with open(filepath, 'r', encoding='utf-8') as json_file:
         return json.load(json_file)
 
 
-def pretty_print_json(json_filename):
-    return pprint(json_content)
+def pretty_print_json(filepath):
+    return print(json.dumps(json_content, ensure_ascii=False, indent=4, sort_keys=False))
 
 
 if __name__ == '__main__':
-    json_content = load_data_json(input('Введите название/адрес файл: '))
-    pretty_print_json(json_content)
-    
+    try:
+        filepath = 'alco_shops.json'
+        json_content = load_data_json(filepath)
+        pretty_print_json(json_content)
+    except (IOError, Exception) as e:
+        print("File is empty or not exist")

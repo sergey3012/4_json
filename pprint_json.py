9467cb34782ae7e476937872e772f78f7733c9be
sergey3012 <33592950+sergey3012@users.noreply.github.com>
2017-11-22 18:25:28 +0300

    внесены изменения

--- pprint_json.py
+++ pprint_json.py
@@ -1,20 +1,27 @@
 import json
-import os
+import sys
 
 
 def load_data_json(filepath):
-    with open(filepath, 'r', encoding='utf-8') as json_file:
-        return json.load(json_file)
+    with open(filepath, 'r', encoding='utf-8') as file:
+        return json.load(file)
 
 
-def pretty_print_json(filepath):
-    return print(json.dumps(json_content, ensure_ascii=False, indent=4, sort_keys=False))
+def pretty_print_json(json_data):
+    if json_data:
+        return print(json.dumps(json_data, ensure_ascii=False, indent=4, sort_keys=False))
 
 
 if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('\nУкажите файл в формате json.\n'
+              'python test.py <path to file>')
+        exit()
     try:
-        filepath = 'alco_shops.json'
-        json_content = load_data_json(filepath)
-        pretty_print_json(json_content)
-    except (IOError, Exception) as e:
-        print("File is empty or not exist")
+        pretty_json = pretty_print_json(load_data_json(sys.argv[1]))
+        print(pretty_json)
+    except (FileNotFoundError, PermissionError) as ex:
+        print('\nОшибка доступа: файл не существует '
+              'или недоступен для чтения.\n' + str(ex))
+    except ValueError as ex:
+        print('\nОшибка json данных!\n' + str(ex))
